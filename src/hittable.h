fbd2369e764a37b6654c5ec831ff6c00fd968e2b
Saim ul Hassan <saimulhassan5@gmail.com>
2022-09-25 02:39:19 +0500

    Added hit_record to store details of ray intersections

--- src/hittable.h
+++ src/hittable.h
@@ -4,11 +4,24 @@
 #include "ray.h"
 #include "vec3.h"
 
+struct hit_record
+{
+    double t;
+    point3 p;
+    vec3 normal;
+    bool front_face;
+
+    void set_face_normal(const ray& r, const vec3& outward_normal)
+    {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
 class hittable
 {
 public:
-    virtual bool hit(const ray& r)=0;
-    virtual color hit_color() const=0;
+    virtual bool hit(const ray& r, double t_min, double t_max, color& attenuation, hit_record& rec)=0;
 };
 
 
--- src/sphere.h
+++ src/sphere.h
@@ -9,7 +9,7 @@ class sphere : public hittable
 public:
     sphere(point3 c, double r, color a) : m_center(c), m_radius(r), m_albedo(a) {}
 
-    virtual bool hit(const ray& r) override 
+    virtual bool hit(const ray& r, double t_min, double t_max, color& attenuation, hit_record& rec) override 
     {
         auto ray_org_to_center = r.origin() - m_center;
         auto a = r.direction().length_squared();
@@ -19,10 +19,25 @@ public:
         auto discriminant = b * b - 4 * a * c;
         if(discriminant < 0) return false;
 
+        auto sqrt_disc = std::sqrt(discriminant);
+        auto root = (-b - sqrt_disc) / (2*a);
+        if(root < t_min || root > t_max) 
+        {
+            root = (-b + sqrt_disc) / (2*a);
+            if(root < t_min || root > t_max) 
+            {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - m_center) / m_radius;
+        rec.set_face_normal(r, outward_normal);
+        attenuation = m_albedo;
         return true;
     }
 
-    virtual color hit_color() const override {return m_albedo;}
 
 private:
     point3 m_center;
