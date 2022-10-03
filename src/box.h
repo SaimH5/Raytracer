305f80da7a1f56ddefd4fedecff7148505e863da
Saim ul Hassan <saimulhassan5@gmail.com>
2022-10-04 01:27:16 +0500

    Added support for rendering boxes

---
+++ src/box.h
@@ -0,0 +1,49 @@
+#ifndef _BOX_h
+#define _BOX_h
+
+#include "utilities.h"
+#include "aarect.h"
+#include "hittable_list.h"
+
+// class material;
+
+class box : public hittable
+{
+public:
+    box() {}
+    box(const point3& p0, const point3& p1, shared_ptr<material> m_ptr);
+
+    virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+    virtual bool bounding_box(double time0, double time1, aabb& output_box) const override 
+    {
+        output_box = aabb(box_min, box_max);
+        return true;
+    }
+private:
+    point3 box_min;
+    point3 box_max;
+    hittable_list sides;
+};
+
+box::box(const point3& p0, const point3& p1, shared_ptr<material> m_ptr)
+{
+    box_min = p0;
+    box_max = p1;
+
+    sides.add(make_shared<xy_rect>(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), m_ptr)); 
+    sides.add(make_shared<xy_rect>(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), m_ptr));
+
+    sides.add(make_shared<xz_rect>(p0.x(), p1.x(), p0.z(), p1.z(), p0.y(), m_ptr)); 
+    sides.add(make_shared<xz_rect>(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), m_ptr)); 
+
+    sides.add(make_shared<yz_rect>(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), m_ptr)); 
+    sides.add(make_shared<yz_rect>(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), m_ptr)); 
+}
+
+bool box::hit(const ray& r, double t_min, double t_max, hit_record& rec) const
+{
+    return sides.hit(r, t_min, t_max, rec);
+}
+
+#endif
