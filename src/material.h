c7e39db5a7878e03bf982f2e9ee825c42a8ee0f6
Saim ul Hassan <saimulhassan5@gmail.com>
2022-10-06 02:30:56 +0500

    Added support for rendering volumes (smoke, fog, etc.)

--- src/material.h
+++ src/material.h
@@ -121,4 +121,21 @@ public:
 private:
     color m_albedo;
 };
+
+
+class isotropic : public material
+{
+public:
+    isotropic(color c) : albedo(make_shared<solid_color>(c)) {}
+    isotropic(shared_ptr<texture> a) : albedo(a) {}
+
+    virtual bool scatter(const ray& r_in, const hit_record& rec, ray& scattered, color& attenuation) const override
+    {
+        scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+private:
+    shared_ptr<texture> albedo;
+};
 #endif
